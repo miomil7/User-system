ba0ddf1cdba8e477c4832d927ee99ef9f8e41d38
Miodrag <miodrag1988@yahoo.com>
2018-08-21 16:16:57 +0200

    Pretrage

--- search.php
+++ search.php
@@ -14,40 +14,64 @@
    <h1 align="center"> <input type = "submit" value="Show all users" name="svi"/></h1>
 </form>
 <?php
-if(isset($_POST["Show all users"])){
-
 
     $link = mysqli_connect("localhost", "root", "", "registracija");
 
-if($link === false){
-    die("ERROR! " . mysqli_connect_error());
-    $sql = "SELECT * FROM 'logovanje'";
-    if($result = mysqli_query($link, $sql)){
-        if(mysqli_num_rows($result) > 0){
+    if ($link === false)
+        die("ERROR! " . mysqli_connect_error());
+if (isset($_POST["svi"])) {
+    $sql = "SELECT username FROM logovanje";
+    if ($result = mysqli_query($link, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
             echo "<table>";
             echo "<tr>";
-            echo "<th>id</th>";
-            echo "<th>Username</th>";
+            echo "<th>All users:</th>";
             echo "</tr>";
-            while($row = mysqli_fetch_array($result)){
+            while ($row = mysqli_fetch_array($result)) {
                 echo "<tr>";
-                echo "<td>" . $row['Id'] . "</td>";
                 echo "<td>" . $row['username'] . "</td>";
                 echo "</tr>";
             }
             echo "</table>";
 
             mysqli_free_result($result);
-        } else{
+        } else {
+            echo "There is no user with that username";
+        }
+    }else {
+        echo "ERROR: Could not able to execute $sql. " .
+            mysqli_error($link);
+    }
+
+if (isset($_POST["submit"])) {
+    $sql = "SELECT username FROM logovanje WHERE username = '{$_POST['imeLog']}'";
+    print_r($sql);
+    if ($result = mysqli_query($link, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>User:</th>";
+            echo "</tr>";
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['imeLog'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+            mysqli_free_result($result);
+        } else {
             echo "No user with that username";
         }
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    } else {
+        echo "ERROR: Could not able to execute $sql. " .
+            mysqli_error($link);
     }
+}
 
 
     mysqli_close($link);
-}
+
 }
 
 
