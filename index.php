227a931520af174c99b6fc7e459e898ccd4414a1
Miodrag <miodrag1988@yahoo.com>
2018-08-21 14:39:05 +0200

    Index page added

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <title>DOBRODOSLI</title><br>
+</head>
+<body>
+<p align="center">DOBRODOSLI</p><br><br>
+<h3 align="center"><a href="LogIn.php"> Ovde </a>   se mozete ulogovati</h3><br>
+<h3 align="center"><a href="registracija.php"> Ovde</a>  se mozete registrovati</h3>
+
+
+</body>
+
+</head>
+</html>
