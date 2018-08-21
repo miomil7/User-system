ae09f03ceea7626d8a4d5d55437953622447f09b
Miodrag <miodrag1988@yahoo.com>
2018-08-21 16:17:19 +0200

    Poboljsano

--- LogIn.php
+++ LogIn.php
@@ -7,19 +7,14 @@
 <h1 align="center">LOG IN</h1><br>
 
 
-<form action="" method="post">
+<form method="post">
     <h2 align="center" <label>Username:</label></h2>
    <h1 align="center"> <input type="text" name="ImeLog"  required="required" placeholder="Username"/></h1><br />
     <h2 align="center"><label>Password:</label></h2>
     <h1 align="center"><input type="password" name="LozinkaLog"  required="required" placeholder="Password"/></h1><br/><br />
+    <h2 align="center"> <input type="submit" value="Log in" name="submit"/></h2><br />
 </form>
-    <form action="search.php" method="POST"></form>
-   <h2 align="center"> <input type="submit" value="Log in" name="submit"/></h2><br />
-</form>
-<form action="search.php" method="POST"></form>
-<form>
     <h2>If you do not have account, please click <a href="registracija.php">here.</a></h2>
-</form>
 <?php
 
 if(isset($_POST["submit"])) {
@@ -34,11 +29,14 @@ if(isset($_POST["submit"])) {
 
 
         if ($_POST['ImeLog'] !== "" && $_POST['LozinkaLog'] !== "") {
-            $conn->exec("SELECT username FROM logovanje WHERE username = '{$_POST['ImeLog']}'
-and password = {$_POST['LozinkaLog']}");
-            $result=mysql_query($conn);
-            header("Location: search.php");
+            $query = $conn->prepare("SELECT username FROM logovanje WHERE username = '{$_POST['ImeLog']}'
+and password = '{$_POST['LozinkaLog']}'");
+            $query->execute();
+            $result = $query->fetchAll();
 
+}
+if(!empty($result)){
+    header("Location: search.php");
         } else {
             echo "Wrong username or password!";
         }
@@ -47,7 +45,7 @@ and password = {$_POST['LozinkaLog']}");
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
-    die();
+
 }
 
 ?>
